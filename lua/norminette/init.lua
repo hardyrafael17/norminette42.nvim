26e0975d8e6375342089e020ab914d9176eabf32
Hardy Jimenez Matos <hardy.rafael17@gmail.com>
2022-12-14 00:02:23 +0100

    chore(norminette.init.lua): updated readme
    update readme

--- lua/norminette/init.lua
+++ lua/norminette/init.lua
@@ -1,22 +1,21 @@
 local M = {}
 
-    vim.api.nvim_create_user_command('Norminette42', 'lua Norminette42()', {})
-
 local utils = require("n-utils.functions")
 M.setup = function(opts)
+  vim.api.nvim_create_user_command('Norminette42', 'lua Norminette42()', {})
   local myluafunc = function ()
     return utils.norminette42(opts.maxErrorsToShow)
   end
 -- Check on buffer enter
   vim.api.nvim_create_autocmd({"BufEnter"}, {
-    pattern = { "*.c" },
+    pattern = { "*.c", "*.h" },
     callback = myluafunc
   })
 
 -- Check on save
   if opts.runOnSave then
     vim.api.nvim_create_autocmd({"BufWritePost"}, {
-    pattern = { "*.c" },
+    pattern = { "*.c", "*.h" },
     callback = myluafunc
   })
   end
